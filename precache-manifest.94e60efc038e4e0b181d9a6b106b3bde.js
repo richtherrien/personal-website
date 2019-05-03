215e2620c27ca5c5f3f2d6e72cf04294bbb3a298
Richard Therrien <richardgtherrien@gmail.com>
2019-05-03 03:41:56 -0400

    Updates

---
+++ precache-manifest.94e60efc038e4e0b181d9a6b106b3bde.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6b1e13394a347339b2d3a1ecb733aa",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "adcad67c32f819ca341a",
+    "url": "/website/static/css/2.14800ccc.chunk.css"
+  },
+  {
+    "revision": "0c17bcf970e71eab1fd4",
+    "url": "/website/static/css/main.fc6d84e0.chunk.css"
+  },
+  {
+    "revision": "adcad67c32f819ca341a",
+    "url": "/website/static/js/2.37876b10.chunk.js"
+  },
+  {
+    "revision": "0c17bcf970e71eab1fd4",
+    "url": "/website/static/js/main.fa50ae93.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "4847ea3562c30f9ee7aad0fb3a595f22",
+    "url": "/website/static/media/richardBackground.4847ea35.jpg"
+  }
+]);
