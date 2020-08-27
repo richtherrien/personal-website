da1f7a9159507b4105066cd61aa74c82f18256a6
Richard Therrien <richardgtherrien@gmail.com>
2020-08-26 22:06:06 -0400

    modified about page, added redirects for deployment

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -17,13 +17,13 @@ class About extends Component {
                         Later, I moved on to work at Startuptive where I worked on creating a demo product to showcase to investors. I developed in C++ and Java using Android Studios and investigated the hardware and software architecture of Internet of Things based devices,
                     </p>
                     <p>
-                        Then as a Software Engineer at Healthy Pets where I used the Ionic framework to update the company’s cross-platform hybrid mobile app using JavaScript, HTML, and CSS to create a new UX/UI and backend based on mockups. I also worked on some smaller projects  using PHP and Python.
+                        Then as a Software Engineer at Healthy Pets where I used the Ionic framework to update the company’s cross-platform hybrid mobile app using JavaScript, HTML, and CSS to create a new UX/UI and backend based on mockups. I also worked on some smaller projects using PHP and Python.
                     </p>
                     <p>
                         At Social Enterprise for Canada I worked with Vue.js and PHP to develop web applications to reduce administrative burdens as well as implementing data analytics and reporting.
                     </p>
                     <p>
-                        Now I am looking for a place to work to utilze my degree in Computer Engineering
+                        Now I am looking for a place to work to utilize my bachelor's degree in Computer Engineering from Ryerson University.
                     </p>
                 </Container>
             </div>
