3b1ad0d970cea937cd23a548183d47702259c02d
Richard Therrien <richardgtherrien@gmail.com>
2020-10-05 19:08:33 -0400

    scaled down background image on home page, removed form in contact page and changed to a link to LinkedIn

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,25 +1,35 @@
-import React, { Component } from 'react';
-import {
-    Navbar,
-    NavLink
-} from 'reactstrap';
-import { FaLinkedin, FaGithub, FaMagnet } from 'react-icons/fa';
+import React, { Component } from "react";
+import { Navbar, NavLink } from "reactstrap";
+import { FaLinkedin, FaGithub, FaMagnet } from "react-icons/fa";
+import "./../../variables.scss";
 
 class Footer extends Component {
     render() {
         return (
             <footer>
-                <div >
+                <div>
                     <Navbar className="bg-dark text-light" expand="md">
                         <FaLinkedin />
-                        <NavLink href="https://www.linkedin.com/in/richard-therrien/">LinkedIn</NavLink>
+                        <NavLink href="https://www.linkedin.com/in/richard-therrien/">
+                            <span className="footer-navigation-text">
+                                LinkedIn&nbsp;
+                            </span>
+                        </NavLink>
                         <FaGithub />
-                        <NavLink href="https://github.com/richtherrien">GitHub</NavLink>
+                        <NavLink href="https://github.com/richtherrien">
+                            <span className="footer-navigation-text">
+                                GitHub&nbsp;
+                            </span>
+                        </NavLink>
                         <FaMagnet />
-                        <NavLink href="https://magnet.whoplusyou.com/cv/ZXlKcGRpSTZJbkpuVFZWRlJVeHFVVmt5Ym1oNVlWWkZUR3RQTTBFOVBTSXNJblpoYkhWbElqb2lhV2xMSzJGa09XSnRNMEpwYTFGUGRrNU5Wbmd5ZWpNNVUxUkhkV1l4U0VKMFVrNU5NeXRuWEM5elFuQk5PVUpVU3pGaGRIVjNjR2N5U2pneWFFVmpNSGRaTTAwMFRFSklUR04xVVRsTU4wVmtlblJqV0cxYU4zSk9SSGx1UlhwdGFucG9OWGRaV0dKdGNVRkpQU0lzSW0xaFl5STZJbUpqWm1SbU5URTJZamt3TjJReU1qYzVNREU1WlRnMk1URTJNMll5TldZMU5XTmxaVGcyT1RVM01UWXlZMkU0WkRWaU5HSXhNalU0TTJReFkyVXhNellpZlE9PQ%3D%3D">Magnet</NavLink>
+                        <NavLink href="https://magnet.whoplusyou.com/cv/ZXlKcGRpSTZJbkpuVFZWRlJVeHFVVmt5Ym1oNVlWWkZUR3RQTTBFOVBTSXNJblpoYkhWbElqb2lhV2xMSzJGa09XSnRNMEpwYTFGUGRrNU5Wbmd5ZWpNNVUxUkhkV1l4U0VKMFVrNU5NeXRuWEM5elFuQk5PVUpVU3pGaGRIVjNjR2N5U2pneWFFVmpNSGRaTTAwMFRFSklUR04xVVRsTU4wVmtlblJqV0cxYU4zSk9SSGx1UlhwdGFucG9OWGRaV0dKdGNVRkpQU0lzSW0xaFl5STZJbUpqWm1SbU5URTJZamt3TjJReU1qYzVNREU1WlRnMk1URTJNMll5TldZMU5XTmxaVGcyT1RVM01UWXlZMkU0WkRWaU5HSXhNalU0TTJReFkyVXhNellpZlE9PQ%3D%3D">
+                            <span className="footer-navigation-text">
+                                Magnet&nbsp;
+                            </span>
+                        </NavLink>
                     </Navbar>
                 </div>
-            </footer >
+            </footer>
         );
     }
 }
--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,32 +1,18 @@
-import React, { Component } from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
-import { Container } from 'reactstrap';
+import React, { Component } from "react";
+import { Container } from "reactstrap";
 
 class Contact extends Component {
     render() {
         return (
             <div className="container-fluid full-height">
                 <Container>
-                    <h1 className="display-4"> Contact Me</h1>
-                    <Form method="POST" action="https://formspree.io/richardgtherrien+personal-websiteform@gmail.com">
-                        <FormGroup>
-                            <Label for="exampleEmail">Email</Label>
-                            <Input type="email" name="email" id="exampleEmail" placeholder="Please provide an email so I can get back to you" required />
-                        </FormGroup>
-
-                        <FormGroup>
-                            <Label for="exampleText">Message</Label>
-                            <Input type="textarea" name="text" id="exampleText" />
-                        </FormGroup>
-
-                        <FormGroup check className="text-md-right">
-                            <Label check>
-                                <Input type="checkbox" required />{' '}
-                                <a href='/terms'>I agree to the terms & conditions </a> 
-                            </Label>
-                        </FormGroup>
-                        <Button type="submit">Submit</Button>
-                    </Form>
+                    <h1 className="display-4">Contact Me</h1>
+                    <p>
+                        Send me a message via&nbsp;
+                        <a href="https://www.linkedin.com/in/richard-therrien/">
+                            LinkedIn.
+                        </a>
+                    </p>
                 </Container>
             </div>
         );
