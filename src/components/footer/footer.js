262e0259881b531be68b954d8d9472c0bfbd14e0
Richard Therrien <richardgtherrien@gmail.com>
2021-04-11 12:56:09 -0400

    removed magnet link from footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -21,12 +21,6 @@ class Footer extends Component {
                                 GitHub&nbsp;
                             </span>
                         </NavLink>
-                        <FaMagnet />
-                        <NavLink href="https://magnet.whoplusyou.com/cv/ZXlKcGRpSTZJbkpuVFZWRlJVeHFVVmt5Ym1oNVlWWkZUR3RQTTBFOVBTSXNJblpoYkhWbElqb2lhV2xMSzJGa09XSnRNMEpwYTFGUGRrNU5Wbmd5ZWpNNVUxUkhkV1l4U0VKMFVrNU5NeXRuWEM5elFuQk5PVUpVU3pGaGRIVjNjR2N5U2pneWFFVmpNSGRaTTAwMFRFSklUR04xVVRsTU4wVmtlblJqV0cxYU4zSk9SSGx1UlhwdGFucG9OWGRaV0dKdGNVRkpQU0lzSW0xaFl5STZJbUpqWm1SbU5URTJZamt3TjJReU1qYzVNREU1WlRnMk1URTJNMll5TldZMU5XTmxaVGcyT1RVM01UWXlZMkU0WkRWaU5HSXhNalU0TTJReFkyVXhNellpZlE9PQ%3D%3D">
-                            <span className="footer-navigation-text">
-                                Magnet&nbsp;
-                            </span>
-                        </NavLink>
                     </Navbar>
                 </div>
             </footer>
